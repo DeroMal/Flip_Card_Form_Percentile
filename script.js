c80571d482fd22aa692eadc3692933b88384f6f3
DeroMal <derrickmal123@gmail.com>
2023-08-11 11:04:40 +0200

    Enhance script.js with detailed comments for clarity
    - Added comprehensive comments throughout script.js to explain the functionality and structure.
    - Improved code readability for better maintainability and future development.
    - Ensured clear understanding for developers and stakeholders reviewing the code.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 // VARIABLES
 // ==========================
 
-// City-Country pairs
+// Define pairs of cities and their corresponding countries.
+// This will be used to generate the game cards.
 const pairs = {
   melbourne: 'australia',
   beijing: 'china',
@@ -10,30 +11,31 @@ const pairs = {
   cairo: 'Egypt',
   madrid: 'spain',
   newyork: 'USA'
-  // Add more pairs similarly
+  // Additional pairs can be added in a similar manner.
 };
 
-let totalCards;
-let cards;
-let flippedCards = [];
-let matchedPairs = 0;
-let startTime;
-let timerInterval;
-let finalElapsedTime;
+// Define game state variables.
+let totalCards;          // Total number of cards in the game.
+let cards;               // NodeList of all card elements.
+let flippedCards = [];   // Array to store currently flipped cards.
+let matchedPairs = 0;    // Counter for matched pairs.
+let startTime;           // Timestamp when the game starts.
+let timerInterval;       // Interval for the game timer.
+let finalElapsedTime;    // Store the elapsed time when all cards are matched.
 
 // ==========================
 // EVENT LISTENERS
 // ==========================
 
+// Initialize the game once the document is fully loaded.
 document.addEventListener('DOMContentLoaded', function () {
-  // Initialize the game
-  showIntroModal();
-  generateCards(pairs);
-  shuffleCards();
-  adjustLayout();
-  attachCardListeners();
+  showIntroModal();      // Display the introductory modal.
+  generateCards(pairs);  // Generate game cards based on city-country pairs.
+  shuffleCards();        // Randomly shuffle the cards on the game board.
+  adjustLayout();        // Adjust the game board layout based on the number of cards.
+  attachCardListeners(); // Attach event listeners to each card.
 
-  // Replay button functionality
+  // Add functionality to the replay button to reset and start a new game.
   const replayButton = document.getElementById('replay');
   replayButton.onclick = function () {
     resetGameState();
@@ -44,72 +46,77 @@ document.addEventListener('DOMContentLoaded', function () {
 // GAME FUNCTIONS
 // ==========================
 
-// Display the intro modal
+// Function to display the introductory modal.
 function showIntroModal() {
   const modal = document.getElementById('introModal');
   modal.style.display = 'block';
 
+  // Start the game and hide the modal when the "Start Game" button is clicked.
   const startGame = document.getElementById('startGame');
   startGame.onclick = function () {
     modal.style.display = 'none';
-    startTimer();
+    startTimer();  // Start the game timer.
   };
 }
 
-// Generate city and country cards
+// Function to generate game cards based on the provided city-country pairs.
 function generateCards(pairs) {
   const gameBoard = document.querySelector('.game-board');
   for (let city in pairs) {
     const country = pairs[city];
-    gameBoard.appendChild(createCard(city));    // City card
-    gameBoard.appendChild(createCard(country)); // Country card
+    gameBoard.appendChild(createCard(city));    // Add city card to the game board.
+    gameBoard.appendChild(createCard(country)); // Add corresponding country card to the game board.
   }
 }
 
-// Create a card (either city or country)
+// Function to create a card element (either city or country).
 function createCard(value) {
   const card = document.createElement('div');
   card.classList.add('card');
-  card.setAttribute('data-value', value);
+  card.setAttribute('data-value', value);  // Use data-value to store the card's value (city or country).
 
+  // Create the back side of the card.
   const cardBack = document.createElement('div');
   cardBack.classList.add('card-back');
   cardBack.textContent = '?';
   card.appendChild(cardBack);
 
+  // Create the front side of the card with the corresponding image.
   const cardFront = document.createElement('div');
   cardFront.classList.add('card-front');
   cardFront.style.backgroundImage = `url('images/${value}.webp')`;
   card.appendChild(cardFront);
 
-  return card;
+  return card;  // Return the created card element.
 }
 
-// Shuffle the cards on the board
+// Function to shuffle the cards on the game board.
 function shuffleCards() {
   const gameBoard = document.querySelector('.game-board');
   const cardsArray = Array.from(gameBoard.children);
-  cardsArray.sort(() => Math.random() - 0.5);
-  cardsArray.forEach(card => gameBoard.appendChild(card));
+  cardsArray.sort(() => Math.random() - 0.5);  // Randomly sort the cards.
+  cardsArray.forEach(card => gameBoard.appendChild(card));  // Append the shuffled cards back to the game board.
 }
 
-// Adjust the layout based on the number of cards
+// Function to adjust the game board layout based on the number of cards.
 function adjustLayout() {
   cards = document.querySelectorAll('.card');
   totalCards = cards.length;
-  const columns = Math.max(3, Math.ceil(Math.sqrt(totalCards)));
+  const columns = Math.max(3, Math.ceil(Math.sqrt(totalCards)));  // Calculate the optimal number of columns.
   const gameBoard = document.querySelector('.game-board');
-  gameBoard.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+  gameBoard.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;  // Set the number of columns in the game board's grid.
 }
 
-// Attach click listeners to cards
+// Function to attach click event listeners to each card.
 function attachCardListeners() {
   cards.forEach(card => {
     card.addEventListener('click', () => {
+      // Flip the card if it's not already flipped and if there are less than 2 cards currently flipped.
       if (!card.classList.contains('flipped') && flippedCards.length < 2) {
         card.classList.add('flipped');
-        flippedCards.push(card);
+        flippedCards.push(card);  // Add the flipped card to the flippedCards array.
 
+        // Check for a match if two cards are flipped.
         if (flippedCards.length === 2) {
           checkMatch();
         }
@@ -118,155 +125,123 @@ function attachCardListeners() {
   });
 }
 
-// Check if two flipped cards are a match
+// Function to check if the two flipped cards are a matching pair.
 function checkMatch() {
   const card1 = flippedCards[0].getAttribute('data-value');
   const card2 = flippedCards[1].getAttribute('data-value');
 
+  // Check if the cards match based on the city-country pairs.
   if (pairs[card1] === card2 || pairs[card2] === card1) {
-    handleMatch();
+    handleMatch();  // Handle the matched cards.
   } else {
-    handleMismatch();
+    handleMismatch();  // Handle the mismatched cards.
   }
 }
 
-// Handle matched cards
-// function handleMatch() {
-//   matchedPairs++;
-//   flippedCards.forEach(card => {
-//     card.style.animation = 'matched 0.5s';
-//     card.addEventListener('animationend', function () {
-//       card.style.animation = '';
-//     });
-//   });
-
-//   const cardFront = flippedCards[1].querySelector('.card-front');
-//   cardFront.addEventListener('transitionend', function onEnd() {
-//     if (matchedPairs === totalCards / 2) {
-//       stopTimer();  // Stop the timer immediately when all cards are matched
-//       // Delay the display of the win modal by 2 seconds (or adjust as needed)
-//       setTimeout(showWinModal, 2000);
-//     }
-//     cardFront.removeEventListener('transitionend', onEnd);
-//   });
-
-//   flippedCards = [];
-// }
-// Handle matched cards
+// Function to handle the scenario when two flipped cards are a match.
 function handleMatch() {
-  matchedPairs++;
+  matchedPairs++;  // Increment the matched pairs counter.
   flippedCards.forEach(card => {
-    card.style.animation = 'matched 0.5s';
+    card.style.animation = 'matched 0.5s';  // Apply a matching animation to the cards.
     card.addEventListener('animationend', function () {
-      card.style.animation = '';
+      card.style.animation = '';  // Remove the animation after it ends.
     });
   });
 
   const cardFront = flippedCards[1].querySelector('.card-front');
   cardFront.addEventListener('transitionend', function onEnd() {
     if (matchedPairs === totalCards / 2) {
-      stopTimer();  // Stop the timer immediately when all cards are matched
-      finalElapsedTime = Date.now() - startTime;  // Store the elapsed time
-      // Delay the display of the win modal by 2 seconds (or adjust as needed)
+      stopTimer();  // Stop the timer immediately when all cards are matched.
+      finalElapsedTime = Date.now() - startTime;  // Store the elapsed time.
+      // Delay the display of the win modal by 2 seconds to give the player a moment to see the matched cards.
       setTimeout(showWinModal, 2000);
     }
-    cardFront.removeEventListener('transitionend', onEnd);
+    cardFront.removeEventListener('transitionend', onEnd);  // Remove the event listener after it's triggered.
   });
 
-  flippedCards = [];
+  flippedCards = [];  // Reset the flippedCards array.
 }
 
-// Handle mismatched cards
+// Function to handle the scenario when two flipped cards are not a match.
 function handleMismatch() {
   flippedCards.forEach(card => {
-    card.style.animation = 'unmatched 0.5s alternate 2';
+    card.style.animation = 'unmatched 0.5s alternate 2';  // Apply a mismatch animation to the cards.
     card.addEventListener('animationend', function () {
-      card.style.animation = '';
+      card.style.animation = '';  // Remove the animation after it ends.
     });
   });
 
+  // After a delay, flip the mismatched cards back to their original state.
   setTimeout(() => {
     flippedCards.forEach(card => card.classList.remove('flipped'));
-    flippedCards = [];
+    flippedCards = [];  // Reset the flippedCards array.
   }, 1000);
 }
 
-// Start the game timer
+// Function to start the game timer.
 function startTimer() {
-  startTime = Date.now();
-  timerInterval = setInterval(updateTimer, 1000);
+  startTime = Date.now();  // Store the current timestamp as the start time.
+  timerInterval = setInterval(updateTimer, 1000);  // Update the timer every second.
 }
 
-// Update the timer display
+// Function to update the timer display.
 function updateTimer() {
-  const elapsed = Date.now() - startTime;
+  const elapsed = Date.now() - startTime;  // Calculate the elapsed time.
   const minutes = Math.floor(elapsed / 60000);
   const seconds = Math.floor((elapsed % 60000) / 1000);
   document.getElementById('timer').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
 
-// Stop the game timer
+// Function to stop the game timer.
 function stopTimer() {
-  clearInterval(timerInterval);
+  clearInterval(timerInterval);  // Clear the timer interval.
 }
 
-// Display the win modal
+// Function to display the win modal after the player matches all card pairs.
 function showWinModal() {
   const modal = document.getElementById('winModal');
-  stopTimer();
+  stopTimer();  // Stop the timer.
 
-  // Display the modal immediately
-  modal.style.display = 'block';
+  modal.style.display = 'block';  // Display the win modal.
 
-  // Show the loading spinner
+  // Create and display a loading spinner in the modal.
   const spinner = document.createElement('div');
   spinner.className = 'loading-spinner';
   modal.querySelector('.modal-content').prepend(spinner);
-  console.log("Spinner added to the modal.");
 
-  // Use the stored finalElapsedTime
+  // Use the stored finalElapsedTime to calculate the player's final game time.
   const minutes = Math.floor(finalElapsedTime / 60000);
   const seconds = Math.floor((finalElapsedTime % 60000) / 1000);
   const timeString = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 
-  // Send the time to the server and get the rank
+  // Send the player's game time to the server to get their rank.
   sendTimeToServer(timeString)
     .then(data => {
-      // Hide the spinner
-      console.log("Data received from server. Hiding spinner.");
+      // Hide the loading spinner.
       spinner.style.display = 'none';
 
-      // Remove existing time and rank displays
-      const existingTimeDisplay = modal.querySelector('.time-display');
-      const existingRankDisplay = modal.querySelector('.rank-display');
-      if (existingTimeDisplay) existingTimeDisplay.remove();
-      if (existingRankDisplay) existingRankDisplay.remove();
-
-      // Update the modal with the time and rank
+      // Update the modal with the player's game time and rank.
       const timeDisplay = document.createElement('p');
       timeDisplay.textContent = `Your time: ${timeString}`;
-      timeDisplay.classList.add('green-text', 'time-display');  // Add the green-text and time-display classes
+      timeDisplay.classList.add('green-text', 'time-display');
 
       const rankDisplay = document.createElement('p');
-      rankDisplay.textContent = data.message; // Display the ranking message
-      rankDisplay.classList.add('green-text', 'rank-display');  // Add the green-text and rank-display classes
+      rankDisplay.textContent = data.message;  // Display the ranking message from the server.
+      rankDisplay.classList.add('green-text', 'rank-display');
 
-      // Insert the time and rank right after the <h2> tag
+      // Insert the game time and rank displays into the modal.
       const h2Element = modal.querySelector('h2');
       h2Element.insertAdjacentElement('afterend', rankDisplay);
       h2Element.insertAdjacentElement('afterend', timeDisplay);
     })
     .catch(error => {
-      // Hide the spinner and show an error message
-      console.log("Error received from server. Hiding spinner.");
+      // If there's an error communicating with the server, hide the spinner and display an error message.
       spinner.style.display = 'none';
       alert(`An error occurred: ${error.message}`);
     });
 }
 
-
-
-// Send the game time to the server and get the player's rank
+// Function to send the player's game time to the server and get their rank.
 async function sendTimeToServer(time) {
   try {
     const response = await fetch('https://flipcardgame-2.derrickmal123.workers.dev/', {
@@ -284,57 +259,57 @@ async function sendTimeToServer(time) {
       throw new Error(`Server responded with ${response.status}: ${response.statusText}`);
     }
 
-    const result = await response.json();
-
-    // Assuming the server returns a property named 'percentile' with the player's percentile rank
-    // and a 'message' property with the ranking message
-    return result;
+    return await response.json();  // Return the server's response.
   } catch (error) {
     console.error('Error sending time:', error);
-    throw error; // Re-throw the error to be caught in the calling function
+    throw error;  // Re-throw the error to be caught in the calling function.
   }
 }
 
-
-// Reset the game state
+// Function to reset the game state and start a new game.
 function resetGameState() {
-  matchedPairs = 0;
-  cards.forEach(card => card.classList.remove('flipped'));
-  shuffleCards();
-  startTimer();
-  document.getElementById('replay').classList.add('hidden');
+  matchedPairs = 0;  // Reset the matched pairs counter.
+  cards.forEach(card => card.classList.remove('flipped'));  // Flip all cards back to their original state.
+  shuffleCards();  // Shuffle the cards on the game board.
+  startTimer();  // Start the game timer.
+  document.getElementById('replay').classList.add('hidden');  // Hide the replay button.
 }
 
 // ==========================
 // FORM FUNCTIONS
 // ==========================
 
-// Validate email format
+// Function to validate an email address format.
 function isValidEmail(email) {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return regex.test(email);
 }
 
-// Handle form submission
+// Event listeners and functions related to the game's form submission.
+
 const firstNameInput = document.getElementById('firstName');
 const emailInput = document.getElementById('email');
 const privacyPolicyCheckbox = document.getElementById('privacyPolicy');
 const sendDataButton = document.getElementById('sendData');
 
+// Enable or disable the "Send Data" button based on the privacy policy checkbox.
 privacyPolicyCheckbox.addEventListener('change', function () {
   sendDataButton.disabled = !this.checked;
   sendDataButton.classList.toggle('disabled-button', !this.checked);
 });
 
+// Handle the form submission.
 sendDataButton.addEventListener('click', async function () {
+  // Validate the email address format.
   if (!isValidEmail(emailInput.value)) {
-    emailInput.classList.add('input-error');
+    emailInput.classList.add('input-error');  // Highlight the email input in case of an invalid format.
     emailInput.addEventListener('input', function() {
-      emailInput.classList.remove('input-error');
+      emailInput.classList.remove('input-error');  // Remove the highlight once the user starts typing.
     });
     return;
   }
 
+  // Prepare the data to be sent to the server.
   const data = {
     type: "saveDetails",
     Name: firstNameInput.value,
@@ -355,10 +330,9 @@ sendDataButton.addEventListener('click', async function () {
     }
 
     const result = await response.json();
-    console.log(result);
 
     if (result.result === 'Success') {
-      handleSuccessfulSubmission();
+      handleSuccessfulSubmission();  // Handle the successful form submission.
     } else {
       alert('Failed to send data. Please try again.');
     }
@@ -368,16 +342,16 @@ sendDataButton.addEventListener('click', async function () {
   }
 });
 
-// Handle successful form submission
+// Function to handle the successful form submission.
 function handleSuccessfulSubmission() {
-  document.querySelector('.game-container').style.display = 'none';
-  document.getElementById('winModal').style.display = 'none';
-  document.getElementById('thankYouModal').style.display = 'block';
+  document.querySelector('.game-container').style.display = 'none';  // Hide the game container.
+  document.getElementById('winModal').style.display = 'none';  // Hide the win modal.
+  document.getElementById('thankYouModal').style.display = 'block';  // Display the "Thank You" modal.
 }
 
-// Restart the game after form submission
+// Restart the game after the form is submitted.
 document.getElementById('restartGame').addEventListener('click', function () {
-  document.getElementById('thankYouModal').style.display = 'none';
-  document.querySelector('.game-container').style.display = 'flex';
-  resetGameState();
+  document.getElementById('thankYouModal').style.display = 'none';  // Hide the "Thank You" modal.
+  document.querySelector('.game-container').style.display = 'flex';  // Display the game container.
+  resetGameState();  // Reset the game state and start a new game.
 });
